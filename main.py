f64c13632ec6c1c37d602aab6f43cb10ff162379
Muhammad Elfikry <melfikry@gmail.com>
2025-01-06 20:31:00 +0700

    add vercel

--- main.py
+++ main.py
@@ -10,22 +10,25 @@ app = Flask(__name__)
 
 @app.route("/api/chat", methods=["POST"])
 def chat():
-    user_input = request.json.get("message")
+    try:
+        user_input = request.json.get("message")
 
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": user_input,
-            }
-        ],
-        model="llama3-8b-8192",
-    )
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input,
+                }
+            ],
+            model="llama3-70b-8192",
+        )
 
-    response_message = chat_completion.choices[0].message.content
+        response_message = chat_completion.choices[0].message.content
 
-    return jsonify({"response": response_message})
+        return jsonify({"response": response_message}), 200
 
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
